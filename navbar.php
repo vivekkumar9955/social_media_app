5751ca77a93678ad1f4ef2a9a8b5132aefe9c94d
vivekkumar9955 <126938288+vivekkumar9955@users.noreply.github.com>
2023-04-18 10:13:13 +0530

    start navbar styling

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<div class="container">
+  <nav>
+   <div class="logo">
+    <h2>Connect to </h2>
+   </div> 
+   <div class="searchbar">
+    <input type="search" placeholder="Find your Social Frinds">
+   </div>
+   <div class="createbtn">
+    create
+   </div>
+   <div class="profile">
+    profile
+   </div>
+  </nav>
+</div>
